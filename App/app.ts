9f1e3e9a4b22a73930902267a4e3dd84e070d203
CAFÉ <pciskolargx@gmail.com>
2026-01-29 16:24:10 -0300

    saving exercise

--- App/app.ts
+++ App/app.ts
@@ -1,5 +1,23 @@
-let a: number[] = [1,2,3];
+enum produtoStatus {
+    Ativo = 1,
+    Inativo = 2,
+    EmBreve = 3,
+}
 
+function checarProdutoStatus(status: produtoStatus){
+    switch(status){
+        case produtoStatus.Ativo:
+            return "Disponivel";
+            break;
+        case produtoStatus.Inativo:
+            return "Indisponivel";
+            break;
+        case produtoStatus.EmBreve:
+            return "Em breve";
+            break;
+        default:
+            return "Erro"
+    }
+}
 
-
-console.log(a);
+console.log(checarProdutoStatus(produtoStatus.Inativo));
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,23 @@
 "use strict";
-let a = [1, 2, 3];
-console.log(a);
+var produtoStatus;
+(function (produtoStatus) {
+    produtoStatus[produtoStatus["Ativo"] = 1] = "Ativo";
+    produtoStatus[produtoStatus["Inativo"] = 2] = "Inativo";
+    produtoStatus[produtoStatus["EmBreve"] = 3] = "EmBreve";
+})(produtoStatus || (produtoStatus = {}));
+function checarProdutoStatus(status) {
+    switch (status) {
+        case produtoStatus.Ativo:
+            return "Disponivel";
+            break;
+        case produtoStatus.Inativo:
+            return "Indisponivel";
+            break;
+        case produtoStatus.EmBreve:
+            return "Em breve";
+            break;
+        default:
+            return "Erro";
+    }
+}
+console.log(checarProdutoStatus(produtoStatus.Inativo));
