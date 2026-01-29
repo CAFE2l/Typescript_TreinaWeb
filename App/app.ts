b23fde17e28986daaaf12505835517764e785430
CAFÉ <pciskolargx@gmail.com>
2026-01-29 15:11:04 -0300

    understanding the core concepts of typescript

---
+++ App/app.ts
@@ -0,0 +1,8 @@
+let nome: string;
+let idade: number;
+let sobrenome: string;
+
+nome = "joao";
+sobrenome = "silva";
+idade = 16;
+console.log(`O nome é ${nome} ${sobrenome} e a idade é ${idade}`);
---
+++ dist/app.js
@@ -0,0 +1,7 @@
+let nome;
+let idade;
+nome = "joao";
+idade = 16;
+console.log(`O nome é ${nome} e a idade é ${idade}`);
+export {};
+//# sourceMappingURL=app.js.map
