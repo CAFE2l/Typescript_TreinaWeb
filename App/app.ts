733165a8d9629b72f14965cb091e5953042c7702
CAFÉ <pciskolargx@gmail.com>
2026-01-29 16:45:37 -0300

    saving updates

--- App/app.ts
+++ App/app.ts
@@ -8,40 +8,41 @@ interface IAnimal{
     morrer(): void;
 }
 
-let cachorro: IAnimal = {
-    nome: "Rex",
-    idade: 0,
-    estaVivo: false,
-    especie: "Canis Lupus Familiaris",
-    nascer(){
+
+
+class Animal implements IAnimal{
+    nome: string;
+    idade: number;
+    estaVivo: boolean;
+
+    constructor(nome: string){
+        this.nome = nome;
+        this.idade = 0;
+        this.estaVivo = false;
+    }
+
+     nascer(){
         this.estaVivo = true;
         return  console.log(`${this.nome} nasceu!`);
-    },
+    };
     crescer(){
         this.idade++;
         console.log(`${this.nome} agora tem ${this.idade} anos.`);
-    },
+    };
     morrer(){
         this.estaVivo = false;
         console.log(`${this.nome} morreu :(`);
-    }
+    };
 }
 
+let cachorro = new Animal('Totó');
 cachorro.nascer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
 cachorro.crescer();
 cachorro.crescer();
 cachorro.morrer();
 
-interface IAnimal{
-    especie: string;
-
-}
+let gato = new Animal("Simba");
+gato.nascer();
+gato.crescer();
+gato.crescer();
+gato.morrer();
--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,33 @@
 "use strict";
-let cachorro = {
-    nome: "Rex",
-    idade: 0,
-    estaVivo: false,
-    especie: "Canis Lupus Familiaris",
+class Animal {
+    constructor(nome) {
+        this.nome = nome;
+        this.idade = 0;
+        this.estaVivo = false;
+    }
     nascer() {
         this.estaVivo = true;
         return console.log(`${this.nome} nasceu!`);
-    },
+    }
+    ;
     crescer() {
         this.idade++;
         console.log(`${this.nome} agora tem ${this.idade} anos.`);
-    },
+    }
+    ;
     morrer() {
         this.estaVivo = false;
         console.log(`${this.nome} morreu :(`);
     }
-};
+    ;
+}
+let cachorro = new Animal('Totó');
 cachorro.nascer();
 cachorro.crescer();
 cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
-cachorro.crescer();
 cachorro.morrer();
+let gato = new Animal("Simba");
+gato.nascer();
+gato.crescer();
+gato.crescer();
+gato.morrer();
